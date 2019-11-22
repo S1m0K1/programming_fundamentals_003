8dd4e9eb711dc9ad7313fc93e28a864f167d5340
Simone Klein <simoneandpaper@gmail.com>
2019-11-22 12:06:44 +0000

    Absplutely nailing understanding of JS

--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -8,4 +8,12 @@ describe("catalogueService", () => {
       expect(catalogueService.countBooksByAuthor("Charles Dickens")).toBe(3);
     });
   });
+
+describe("catalogueService.checkBookByTitle", () => {
+  test("returns true if the book exists", () => {
+    expect(catalogueService.checkBookByTitle("The Assassination of Margaret Thatcher")).toBe(true);
+    expect(catalogueService.checkBookByTitle("Harry Potter and the Half-Blood Prince")).toBe(false);
+    });
+  });
 });
+
